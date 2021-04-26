dfb02f21fccfcc7c6a4d04e3f3af40a0adf3b4c5
noia-00 <owner@MA4-0036.local>
2021-04-26 10:13:17 +0900

    バリデーション・マイグレーション等

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  with_options presence: true do
+    validates :nickname
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+  end
+end
